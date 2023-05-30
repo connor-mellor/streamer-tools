2fbfc82220efd78acc0ec663ea607f4d590c0f76
connor-mellor <Connor_Mellor@hotmail.com>
2023-05-30 23:26:19 +0100

    fix: fix path issues

--- scriptjs.js
+++ scriptjs.js
@@ -5,14 +5,14 @@ const widgets = [
     {
         name: "Tags Converter",
         href: "./widgets/TagsConverter/index.html"
-    },
+    }
 ];
 
 widgets.forEach(widget => {
     const tile = tileTemplate.cloneNode(true)
-        .content.querySelector('.tile');
-    const nameElement = tile.querySelector('a  p');
-    const hrefElement = tile.querySelector('a')
+        .content.querySelector('a');
+    const nameElement = tile.querySelector('div > p');
+    const hrefElement = tile;
     
     nameElement.innerText = widget.name;    
     hrefElement.href = widget.href;
